27f3caf02255d609fbe6cc2ef4189c4eb74532c0
jayfe <jayfe@mail.com>
2021-11-29 11:31:10 +0100

    First commit

---
+++ robot-shop/server/java/com/example/jayfe/robotshop/RobotShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jayfe.robotshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
